495a51735254c3c1d1c197fb7d3fe569dc2a0c13
kelompokptik <cyberlast173@gmail.com>
2024-09-18 20:11:41 +0700

    Pertemuan Pertama python

---
+++ index.py
@@ -0,0 +1,51 @@
+# # TIPE DATA INTEGER
+# data_integer = 1
+# print(data_integer, type(data_integer))
+
+# # TIPE DATA FLOAT
+# data_float = 10.5
+# print(data_float, type(data_float))
+
+# # TIPE DATA STRING
+# data_string = "Udin sedunia"
+# print(data_string, type(data_string))
+
+# # TIPE DATA BOOLEAN (True / False)
+# data_boolean = True
+# print(data_boolean, type(data_boolean))
+
+
+# # Operator Aritmatika
+# a = 10
+# b = 20
+
+# c = 31
+# d = 5
+
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a ** b)
+# print(c % d)
+# print(c // d)
+
+# OPERATOR LOGIKA
+# and
+# or
+# not
+
+# print(True and True)
+# print(True or False)
+# print(not True)
+
+# OPERATOR RELASIONAL
+a = 5
+b = 5
+c = 10
+
+print(a == b)
+print(a != c)
+print(a < c)
+print(a > c)
+print(a <= c)
